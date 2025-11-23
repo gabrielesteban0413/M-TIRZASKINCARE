61e9703327054329bb76d781cad19c6bb99d9d7f
Gabriel <gabrielesteban0413@gmail.com>
2025-11-22 19:27:24 -0500

    Add unoptimized images

--- bcms.config.cjs
+++ bcms.config.cjs
@@ -1,10 +1,13 @@
-// bcms.config.js (o el nombre de tu archivo de configuración de BCMS)
+/**
+ * @type {import('@thebcms/cli/config').BCMSConfig}
+ */
 module.exports = {
-  cms: {
-    origin: process.env.BCMS_CMS_ORIGIN || 'https://cloud-api.thebcms.com',
-    key: {
-      id: process.env.BCMS_API_KEY_ID,
-      secret: process.env.BCMS_API_KEY_SECRET,
+    client: {
+        orgId: process.env.BCMS_ORG_ID,
+        instanceId: process.env.BCMS_INSTANCE_ID,
+        apiKey: {
+            id: process.env.BCMS_API_KEY_ID,
+            secret: process.env.BCMS_API_KEY_SECRET,
+        },
     },
-  },
 };
